86009b31030b8018effbca1538be4eeb6844825d
Crazydodo123 <peizheg@gmail.com>
2025-02-27 02:42:45 -0500

    add header

---
+++ app/components/header.tsx
@@ -0,0 +1,38 @@
+const Header = () => {
+
+    return (
+        <header className="m-3">
+            <nav className="flex py-2.5 px-8 bg-black/10 rounded-lg justify-between">
+                <div>
+                    <a href="/">
+                        <img  className="h-6.5" src="/main-camelai.webp" alt="camelAI logo"/>
+                    </a>
+                </div>
+                <div>
+                    <ul className="flex">
+                        <li className="px-4 font-semibold">
+                            <a className="relative group text-white no-underline">
+                                <p className="group-hover:-translate-y-0.25">Blog</p>
+                            <span className="absolute left-0 -bottom-0.25 w-0 h-0.5 bg-gradient-to-r from-emerald-100 to-purple-900 transition-all duration-400 group-hover:w-full"></span>
+                            </a>
+                        </li>
+                        <li className="px-4 font-semibold">
+                            <a className="relative group text-white no-underline">
+                                <p className="group-hover:-translate-y-0.25">Pricing</p>
+                            <span className="absolute left-0 -bottom-0.25 w-0 h-0.5 bg-gradient-to-r from-emerald-100 to-purple-900 transition-all duration-400 group-hover:w-full"></span>
+                            </a>
+                        </li>
+                        <li className="px-4 font-semibold">
+                            <a className="relative group text-white no-underline">
+                                <p className="group-hover:-translate-y-0.25">Log in</p>
+                            <span className="absolute left-0 -bottom-0.25 w-0 h-0.5 bg-gradient-to-r from-emerald-100 to-purple-900 transition-all duration-400 group-hover:w-full"></span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
