9f9d699dbf911b3dcb55d63cfd35bcbf6dcb21f4
Peizhe Guan <peizheg@gmail.com>
2025-02-28 19:02:10 -0500

    add hero section

---
+++ app/components/hero.tsx
@@ -0,0 +1,58 @@
+import { FaLock, FaShieldAlt } from 'react-icons/fa';
+import { SiYcombinator } from 'react-icons/si';
+
+export default function Hero() {
+  return (
+    <>
+        {/* The blur background */}
+        <div 
+            className="absolute top-0 w-full h-13/10 -z-1"
+            style={{ background: 'linear-gradient(to top right, #58F1FF, #5F83F0, #E54BDA, #FF8D29, #F83A31, #35012F)' }}>
+        </div>
+        <div className="absolute top-0 w-full h-13/10 bg-[#0000008e] backdrop-blur-[150px] -z-1"></div>
+
+
+        {/* The hero content */}
+        <div>
+            <div className="text-center mt-10 mb-14">
+                <h1 className="text-white text-6xl font-bold mb-10">The AI Data Analyst</h1>
+                <p className="text-white text-[25px] max-w-3xl mx-auto">
+                    Ask questions, get instant charts and insights from your data.
+                </p>
+            </div>
+
+        <div className="flex flex-wrap justify-center gap-4 mb-16">
+          <a className="bg-white text-gray-800 text-lg font-semibold px-6 py-3 w-44 h-11 rounded-[20px] flex items-center justify-between font-medium hover:-translate-y-0.5 hover:duration-500 hover:ease-[cubic-bezier(0.4, 0, 0.2, 1)] hover:bg-gradient-to-r from-[#58f1ff63] via-[#5f83f04f] to-[#e54bdb49]">
+            Try free
+            <img className="h-3.5" src='/arrow_right.webp' alt="arrow right" />
+          </a>
+          <a 
+            href="/book-demo" 
+            className="bg-transparent text-lg font-semibold border-2 border-[#ffffff68] text-white px-5 py-3 w-44 h-11 rounded-[20px] flex items-center justify-between font-medium hover:bg-white/10 transition duration-300"
+          >
+            Book demo
+            <img className="fill-white h-3.5" src='/arrow_right.webp' alt="arrow right" />
+          </a>
+        </div>
+
+        <div className="flex flex-wrap justify-center gap-2 mb-12">
+          <div className="bg-white/10 text-white px-2.5 py-2 rounded-xl flex items-center backdrop-blur-sm">
+            <SiYcombinator className="text-orange-500 mr-2" />
+            <p className='font-light'>Y Combinator backed</p>
+          </div>
+          <div className="bg-white/10 text-white px-2.5 py-2 rounded-xl flex items-center backdrop-blur-sm">
+            <FaLock className="mr-2" />
+            <p className='font-light'>Enterprise Security</p>
+          </div>
+          <div className="bg-white/10 text-white px-2.5 py-2 rounded-xl flex items-center backdrop-blur-sm">
+            <FaShieldAlt className="mr-2" />
+            <p className='font-light'>100% Data Privacy</p>
+          </div>
+        </div>
+
+        
+    </div>
+    </>
+
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import Header from "./components/header"
+import Hero from "./components/hero"
 
 const Page = () => {
-  return <Header />
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  )
 }
 
 export default Page
